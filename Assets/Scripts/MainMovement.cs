d6257ae944d8cd7d07ac1eba81c6405999db56af
gonzalo1@uw.edu <gonzalo1@uw.edu>
2020-11-04 04:58:33 -0800

    include a dash sound when spacebar is pressed

--- Assets/Scripts/MainMovement.cs
+++ Assets/Scripts/MainMovement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class MainMovement : MonoBehaviour {
   private float kliffSpeed = 25f;
+  public AudioSource source;
   // Start is called before the first frame update
   void Start () {
-
+    source = GetComponent<AudioSource> ();
   }
 
   // Update is called once per frame
@@ -19,5 +20,10 @@ public class MainMovement : MonoBehaviour {
       transform.position += transform.up * (kliffSpeed * Time.smoothDeltaTime);
     if (Input.GetKey (KeyCode.S)) // left direction movement
       transform.position -= transform.up * (kliffSpeed * Time.smoothDeltaTime);
+
+    if (Input.GetKeyDown (KeyCode.Space)) {
+      // do the dash
+      source.Play ();
+    }
   }
 }
