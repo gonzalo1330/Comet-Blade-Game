91a3fb10d6c0f2b3cfaac5f94722421e7bd3ecd4
gonzalo1@uw.edu <gonzalo1@uw.edu>
2020-11-06 02:56:53 -0800

    included dash movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,183 +2,145 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     private float movementInputDirection;
-    /*private float dashTimeLeft;
-    private float lastImageXpos;
-    private float lastDash = -100f;*/
-
-    private int amountOfJumpsLeft;
     private int facingDirection = 1;
-
     private bool isFacingRight = true;
     private bool isWalking;
+    public float movementSpeed;
+    public float jumpForce;
     private bool isGrounded;
-    private bool canJump;
-    //private bool isDashing;
+    public float groundCheckRadius;
+
+    private bool isDashing;
+    private const float defaultDashTime = 5.0f;
+    private const float defaultSpeed = 5.0f;
+    private const float defaultForce = 16.0f;
 
     private Rigidbody2D rb;
     private Animator anim;
 
-     public int amountOfJumps = 1;
-
-    public float movementSpeed = 10.0f;
-    public float jumpForce = 16.0f;
-    public float groundCheckRadius;
-    /*public float dashTime;
-    public float dashSpeed;
-    public float distanceBetweenImages;
-    public float dashCooldown;*/
+    private bool canJump;
+    private int amountOfJumpsLeft;
+    public int amountOfJumps = 1;
 
     public Transform groundCheck;
 
     public LayerMask whatIsGround;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+    void Start () {
+        rb = GetComponent<Rigidbody2D> ();
+        anim = GetComponent<Animator> ();
         amountOfJumpsLeft = amountOfJumps;
+        movementSpeed = defaultSpeed;
+        jumpForce = defaultForce;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        CheckInput();
-        CheckMovementDirection();
-        UpdateAnimations();
-        CheckIfCanJump();
-        //CheckDash();
+    void Update () {
+        CheckInput ();
+        CheckMovementDirection ();
+        UpdateAnimations ();
+        CheckIfCanJump ();
     }
 
-    private void FixedUpdate()
-    {
-        ApplyMovement();
-        CheckSurroundings();
+    private void FixedUpdate () {
+        ApplyMovement ();
+        CheckSurroundings ();
     }
 
-    private void CheckSurroundings()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    private void CheckSurroundings () {
+        isGrounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
-    private void CheckIfCanJump()
-    {
-        if((isGrounded && rb.velocity.y <= 0))
-        {
+    private void CheckIfCanJump () {
+        if ((isGrounded && rb.velocity.y <= 0)) {
             amountOfJumpsLeft = amountOfJumps;
         }
 
-        if(amountOfJumpsLeft <= 0)
-        {
+        if (amountOfJumpsLeft <= 0) {
             canJump = false;
-        }
-        else
-        {
+        } else {
             canJump = true;
         }
-      
+
     }
 
-    private void CheckMovementDirection()
-    {
-        if(isFacingRight && movementInputDirection < 0)
-        {
-            Flip();
+    private void CheckMovementDirection () {
+        if (isFacingRight && movementInputDirection < 0) {
+            Flip ();
+        } else if (!isFacingRight && movementInputDirection > 0) {
+            Flip ();
         }
-        else if(!isFacingRight && movementInputDirection > 0)
-        {
-            Flip();
-        }
-        
-        if(rb.velocity.x > 0.01f || rb.velocity.x < -0.01f)
-        {
+
+        if (rb.velocity.x > 0.01f || rb.velocity.x < -0.01f) {
             isWalking = true;
-        }
-        else
-        {
+        } else {
             isWalking = false;
         }
     }
 
-    private void UpdateAnimations()
-    {
-        anim.SetBool("isWalking", isWalking);
-        anim.SetBool("isGrounded", isGrounded);
-        anim.SetFloat("yVelocity", rb.velocity.y);
+    private void UpdateAnimations () {
+        anim.SetBool ("isWalking", isWalking);
+        anim.SetBool ("isGrounded", isGrounded);
+        anim.SetFloat ("yVelocity", rb.velocity.y);
     }
 
-    private void CheckInput()
-    {
-        movementInputDirection = Input.GetAxisRaw("Horizontal");
+    private void CheckInput () {
+        movementInputDirection = Input.GetAxisRaw ("Horizontal");
 
-        if(Input.GetButtonDown("Jump")) {
-            Jump();
+        if (Input.GetButtonDown ("Jump")) {
+            Jump ();
         }
 
-        /*
-        if(Input.GetButtonDown("Dash")) {
-            if(Time.time >= (lastDash + dashCooldown)) {
-                AttemptToDash();
-            }
+        CheckDash ();
+    }
+
+    private void CheckDash () {
+        if (isDashing) {
+            jumpForce = 33;
+            movementSpeed = 21;
+        } else {
+            movementSpeed = defaultSpeed;
+            jumpForce = defaultForce;
         }
-        */
+
+        if (Input.GetKey (KeyCode.L))
+            isDashing = false;
     }
 
-/*
-    private void AttemptToDash() {
-        isDashing = true;
-        dashTimeLeft = dashTime;
-        lastDash = Time.time;
-
-        PlayerAfterImagePool.Instance.GetFromPool();
-        lastImageXpos = transform.position.x;
-    }
-
-
-    private void CheckDash() {
-        if(isDashing) {
-            if(dashTimeLeft > 0) {
-                rb.velocity = new Vector2(dashSpeed * facingDirection, rb.velocity.y);
-                dashTimeLeft -= Time.deltaTime;
-
-                if(Mathf.Abs(transform.position.x - lastImageXpos) > distanceBetweenImages) {
-                    PlayerAfterImagePool.Instance.GetFromPool();
-                    lastImageXpos = transform.position.x;
-                }
-            }
-
-            if(dashTimeLeft <= 0) {
-                isDashing = false;
-            }
-        }
-    }
-*/
-    private void Jump() 
-    {
+    private void Jump () {
         if (canJump) {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-             amountOfJumpsLeft--;
+            rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
         }
     }
 
-
-    private void ApplyMovement()
-    {
-        rb.velocity = new Vector2(movementSpeed * movementInputDirection, rb.velocity.y);
+    private void ApplyMovement () {
+        rb.velocity = new Vector2 (movementSpeed * movementInputDirection, rb.velocity.y);
     }
 
-    private void Flip()
-    {
+    private void Flip () {
         isFacingRight = !isFacingRight;
-        transform.Rotate(0.0f, 180.0f, 0.0f);
+        transform.Rotate (0.0f, 180.0f, 0.0f);
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    private void OnDrawGizmos () {
+        Gizmos.DrawWireSphere (groundCheck.position, groundCheckRadius);
     }
 
+    public void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.gameObject.name == "PowerUp") {
+            isDashing = true;
+            GameObject diamond = GameObject.Find ("PowerUp");
+            Destroy (diamond);
+            StartCoroutine (SetDashToFalse ());
+        }
+    }
+
+    IEnumerator SetDashToFalse () {
+        yield return new WaitForSeconds (defaultDashTime); // waits before continuing in seconds
+        isDashing = false;
+    }
 }
