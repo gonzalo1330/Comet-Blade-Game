504fa016d997322ad0b9c7ee7a26866084d4bf9f
Zach Shim <shimz2@uw.edu>
2020-11-14 17:39:40 -0800

    Updates

--- Assets/Scripts/Player/FireBallBehavior.cs
+++ Assets/Scripts/Player/FireBallBehavior.cs
@@ -9,6 +9,32 @@ public class FireBallBehavior : MonoBehaviour {
     private static Cursor sCursor = null;
     private Bounds mWorldBounds;
     static public void SetCursor(Cursor g) { sCursor = g; }
+
+    public float groundCheckRadius;
+    public float wallCheckDistance;
+
+    private AttackDetails attackDetails;
+
+    [SerializeField]
+    private Transform attack1HitBoxPos;
+
+    [SerializeField]
+    private float stunDamageAmount = 1f;
+
+    [SerializeField]
+    private float inputTimer, attack1Radius, attack1Damage;
+
+    [SerializeField]
+    private LayerMask whatIsDamageable;
+
+    public Transform groundCheck;
+    public Transform wallCheck;
+
+    private bool isGrounded;
+    private bool isTouchingWall;
+
+    public LayerMask whatIsGround;
+
     // Start is called before the first frame update
     void Start() {
         mLifeCount = kLifeTime;
@@ -19,9 +45,34 @@ public class FireBallBehavior : MonoBehaviour {
     void Update() {
         transform.position += transform.right * (kEggSpeed * Time.smoothDeltaTime);
         mLifeCount -= Time.smoothDeltaTime;
-        //if (!mWorldBounds.Contains(transform.position)) {
-        //    Destroy(transform.gameObject);  // kills self
-        //}
+
+        CheckSurroundings();
+        CheckAttackHitBox();
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        isTouchingWall = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, whatIsGround);
+
+        if(isGrounded || isTouchingWall) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void CheckAttackHitBox()
+    {
+        Collider2D[] detectedObjects = Physics2D.OverlapCircleAll(attack1HitBoxPos.position, attack1Radius, whatIsDamageable);
+
+        attackDetails.damageAmount = attack1Damage;
+        attackDetails.position = transform.position;
+        attackDetails.stunDamageAmount = stunDamageAmount;
+
+        foreach (Collider2D collider in detectedObjects)
+        {
+            collider.transform.parent.SendMessage("Damage", attackDetails);
+            //Instantiate hit particle
+        }
     }
 
 
@@ -29,5 +80,10 @@ public class FireBallBehavior : MonoBehaviour {
         print(collision.gameObject.name);
         GameObject parent = collision.transform.parent.gameObject;
         //parent.GetComponent<BasicEnemyController>().TakeDamage(5f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attack1HitBoxPos.position, attack1Radius);
     }
 }
