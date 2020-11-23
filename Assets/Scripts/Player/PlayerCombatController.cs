84f615e0965d7a243cb522b1cb8ace4bed4dea27
gonzalo1@uw.edu <gonzalo1@uw.edu>
2020-11-23 15:32:29 -0800

    updating to include Kelvin changes

--- Assets/Scripts/Player/PlayerCombatController.cs
+++ Assets/Scripts/Player/PlayerCombatController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCombatController : MonoBehaviour
-{
+public class PlayerCombatController : MonoBehaviour {
     [SerializeField]
     private bool combatEnabled;
     private bool isGrounded;
@@ -15,7 +14,7 @@ public class PlayerCombatController : MonoBehaviour
     private Transform attack1HitBoxPos;
     [SerializeField]
     private LayerMask whatIsDamageable;
-    
+
     private bool gotInput, isAttacking, isFirstAttack;
 
     private float lastInputTime = Mathf.NegativeInfinity;
@@ -29,35 +28,30 @@ public class PlayerCombatController : MonoBehaviour
 
     public GameObject bulletPrefab;
 
-    private void Start()
-    {
-        anim = GetComponent<Animator>();
-        anim.SetBool("canAttack", combatEnabled);
-        PC = GetComponent<PlayerController>();
-        PS = GetComponent<PlayerStats>();
+    private void Start () {
+        anim = GetComponent<Animator> ();
+        anim.SetBool ("canAttack", combatEnabled);
+        PC = GetComponent<PlayerController> ();
+        PS = GetComponent<PlayerStats> ();
     }
 
-    private void Update()
-    {
-        Debug.Log("Player health is " + PS.GetHealth());
-        CheckCombatInput();
-        CheckAttacks();
+    private void Update () {
+        Debug.Log ("Player health is " + PS.GetHealth ());
+        CheckCombatInput ();
+        CheckAttacks ();
     }
-    
-    private void FixedUpdate() {
-        CheckSurroundings();
+
+    private void FixedUpdate () {
+        CheckSurroundings ();
     }
 
     private void CheckSurroundings () {
         isGrounded = Physics2D.OverlapCircle (PC.groundCheck.position, PC.groundCheckRadius, PC.whatIsGround);
     }
 
-    private void CheckCombatInput()
-    {
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            if (combatEnabled)
-            {
+    private void CheckCombatInput () {
+        if (Input.GetKeyDown (KeyCode.W)) {
+            if (combatEnabled) {
                 //Attempt combat
                 gotInput = true;
                 lastInputTime = Time.time;
@@ -65,96 +59,82 @@ public class PlayerCombatController : MonoBehaviour
         }
     }
 
-    private void CheckAttacks()
-    {
-        if (gotInput)
-        {
+    private void CheckAttacks () {
+        if (gotInput) {
             //Perform Attack1
-            if (!isAttacking)
-            {
+            if (!isAttacking) {
                 // set variables
                 gotInput = false;
                 isAttacking = true;
                 isFirstAttack = !isFirstAttack;
-                
-                if(isGrounded) {
+
+                if (isGrounded) {
                     // set animatinos
-                    anim.SetBool("attack1", true);
-                }
-                else if (!isGrounded) {
-                    anim.SetBool("attack2", true);
+                    anim.SetBool ("attack1", true);
+                } else if (!isGrounded) {
+                    anim.SetBool ("attack2", true);
                     GameObject e = Instantiate (bulletPrefab);
                     e.transform.localPosition = transform.localPosition;
                     e.transform.localRotation = transform.localRotation;
                 }
-                anim.SetBool("attack1", true);
-                anim.SetBool("firstAttack", isFirstAttack);
-                anim.SetBool("isAttacking", isAttacking);
+                anim.SetBool ("attack1", true);
+                anim.SetBool ("firstAttack", isFirstAttack);
+                anim.SetBool ("isAttacking", isAttacking);
             }
         }
 
-        if(Time.time >= lastInputTime + inputTimer)
-        {
+        if (Time.time >= lastInputTime + inputTimer) {
             //Wait for new input
             gotInput = false;
         }
     }
 
-    private void CheckAttackHitBox()
-    {
-        Collider2D[] detectedObjects = Physics2D.OverlapCircleAll(attack1HitBoxPos.position, attack1Radius, whatIsDamageable);
+    private void CheckAttackHitBox () {
+        Collider2D[] detectedObjects = Physics2D.OverlapCircleAll (attack1HitBoxPos.position, attack1Radius, whatIsDamageable);
 
         attackDetails.damageAmount = attack1Damage;
         attackDetails.position = transform.position;
         attackDetails.stunDamageAmount = stunDamageAmount;
 
-        foreach (Collider2D collider in detectedObjects)
-        {
-            collider.transform.parent.SendMessage("Damage", attackDetails);
+        foreach (Collider2D collider in detectedObjects) {
+            collider.transform.parent.SendMessage ("Damage", attackDetails);
             //Instantiate hit particle
         }
     }
 
-    private void FinishAttack1()
-    {
+    private void FinishAttack1 () {
         isAttacking = false;
-        anim.SetBool("isAttacking", isAttacking);
-        anim.SetBool("attack1", false);
-        anim.SetBool("attack2", false);
+        anim.SetBool ("isAttacking", isAttacking);
+        anim.SetBool ("attack1", false);
+        anim.SetBool ("attack2", false);
     }
 
-    private void Damage(AttackDetails attackDetails)
-    {
-        if (!PC.GetDashStatus())
-        {
+    private void Damage (AttackDetails attackDetails) {
+        if (!PC.GetDashStatus ()) {
             int direction;
 
             // decrease health
 
-            if (attackDetails.position.x < transform.position.x)
-            {
+            if (attackDetails.position.x < transform.position.x) {
                 direction = 1;
-            }
-            else
-            {
+            } else {
                 direction = -1;
             }
-            PS.DecreaseHealth(attackDetails.damageAmount);
-            PC.Knockback(direction);
-        }        
+            PS.DecreaseHealth (attackDetails.damageAmount);
+            PC.Knockback (direction);
+        }
     }
 
     void OnCollisionEnter2D (Collision2D collision) {
         // collision with enemy missile
         if (collision.gameObject.tag == "Hazard") {
-            Debug.Log("Hit by ball");
-            PS.DecreaseHealth(10f);
+            Debug.Log ("Hit by ball");
+            PS.DecreaseHealth (10f);
         }
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(attack1HitBoxPos.position, attack1Radius);
+    private void OnDrawGizmos () {
+        Gizmos.DrawWireSphere (attack1HitBoxPos.position, attack1Radius);
     }
 
 }
--- Assets/Scripts/Utilities/LevelObjects/KillPlane.cs
+++ Assets/Scripts/Utilities/LevelObjects/KillPlane.cs
@@ -3,43 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class KillPlane : MonoBehaviour
-{
-    public GameObject respawnPoint = null;
-    public PlayerStats PS;
+public class KillPlane : MonoBehaviour {
+  public GameObject respawnPoint = null;
+  public PlayerStats PS;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Assert(respawnPoint != null);
-    }
+  // Start is called before the first frame update
+  void Start () {
+    Debug.Assert (respawnPoint != null);
+  }
 
-    public void SetRespawn(GameObject SpawnPoint)
-    {
-        respawnPoint = SpawnPoint;
-    }
+  public void SetRespawn (GameObject SpawnPoint) {
+    respawnPoint = SpawnPoint;
+  }
 
-    private void OnCollisionEnter2D(Collision2D collider)
-    {
-        if(collider.gameObject.layer == 11) {
-            if(PS.GetCheckpointStatus()) {
-                PS.RespawnAtLastCheckPoint();
-            }
-            else {
-                Vector3 pos = respawnPoint.transform.position;
-                collider.transform.position = pos;
-                if(PS.GetHealth() < 50f) {
-                    PS.SetHealth();
-                }
-            }
+  private void OnCollisionEnter2D (Collision2D collider) {
+    if (collider.gameObject.layer == 11) {
+      if (PS.GetCheckpointStatus ()) {
+        PS.RespawnAtLastCheckPoint ();
+      } else {
+        Vector3 pos = respawnPoint.transform.position;
+        collider.transform.position = pos;
+        if (PS.GetHealth () < 50f) {
+          PS.SetHealth ();
         }
-        else {
-            if (collider.gameObject.layer != 12)
-            {
-                Destroy(collider.gameObject);
-            }
-        }
+      }
+    } else {
+      if (collider.gameObject.layer != 12) {
+        Destroy (collider.gameObject);
+      }
     }
+  }
 }
-
-
