e9d5a473283eba1bf52b9e72dff355c0d7e607e0
Zach Shim <shimz2@uw.edu>
2020-11-29 04:04:57 -0800

    Level Improvements

---
+++ Assets/Scripts/Trap/SwordDamage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "Player")
+        collision.gameObject.GetComponent<PlayerStats>().DecreaseHealth(10f);
+    }
+}
