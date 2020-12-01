6f78e21f8677d86a9f8f6b818ae525f7f017d109
bb10789 <chakhim.kam@gmail.com>
2020-11-30 16:22:18 -0800

    boss fixed

--- Assets/Scripts/Boss/Boss_Run.cs
+++ Assets/Scripts/Boss/Boss_Run.cs
@@ -10,8 +10,15 @@ public class Boss_Run : StateMachineBehaviour
     public Boss boss;
     public float attackRange = 5f;
     public float attackCooldown = 2f;
+    public Transform wallCheck;
+    public LayerMask whatIsGround;
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+
+    public bool WallCheck() {
+        return Physics2D.Raycast(wallCheck.position, wallCheck.right, 1f, whatIsGround);
+    }
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        wallCheck = GameObject.FindGameObjectWithTag("BossWallCheck").transform;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = animator.GetComponent<Rigidbody2D>();
         boss = animator.GetComponent<Boss>();
@@ -22,6 +29,7 @@ public class Boss_Run : StateMachineBehaviour
         boss.LookAtPlayer();
         Vector2 target = new Vector2(player.position.x, rb.position.y);
         Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        if (!WallCheck())
         rb.MovePosition(newPos);
         if (Vector2.Distance(player.position, rb.position) <= attackRange && attackCooldown <= 0) {
             animator.SetTrigger("Attack");
--- Assets/Scripts/Trap/SwordDamage.cs
+++ Assets/Scripts/Trap/SwordDamage.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class SwordDamage : MonoBehaviour
 {
+    public AttackDetails attackDetails;
     // Start is called before the first frame update
     void Start()
     {
-        
+        attackDetails.damageAmount = 10f;
     }
 
     // Update is called once per frame
@@ -18,6 +19,6 @@ public class SwordDamage : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.name == "Player")
-        collision.gameObject.GetComponent<PlayerStats>().DecreaseHealth(10f);
+            collision.gameObject.GetComponent<Player>().Damage(attackDetails);
     }
 }
