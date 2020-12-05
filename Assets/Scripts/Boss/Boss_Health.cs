577429a903e261d26b0f7409c45824a71a6ac947
bb10789 <chakhim.kam@gmail.com>
2020-12-04 17:33:32 -0800

    Boss Color

--- Assets/Scripts/Boss/Boss_Health.cs
+++ Assets/Scripts/Boss/Boss_Health.cs
@@ -9,10 +9,16 @@ public class Boss_Health : MonoBehaviour {
     public bool special1 = false;
     public float flyTimer = 4f;
     public GameObject player;
+    bool hurt = false;
+    Color prevColor;
+    GameObject head;
 
     // Start is called before the first frame update
     void Start() {
         Debug.Assert(player != null);
+        head = GameObject.Find("Head");
+        var renderer = head.GetComponent<Renderer>();
+        prevColor = renderer.material.GetColor("_Color");
     }
 
     // Update is called once per frame
@@ -33,6 +39,7 @@ public class Boss_Health : MonoBehaviour {
         if (bossHealth == 0) {
             Die();
         }
+        HitColor();
     }
     public void Die() {
         player.GetComponent<Player>().EndOfLevel();
@@ -51,5 +58,19 @@ public class Boss_Health : MonoBehaviour {
     }
     public void Damage(AttackDetails damageDetail) {
         bossHealth -= damageDetail.damageAmount;
+        hurt = true;
+        var renderer = head.GetComponent<Renderer>();
+        prevColor = renderer.material.GetColor("_Color");
+        HitColor();
+    }
+
+    public void HitColor() {
+        var renderer = head.GetComponent<Renderer>();
+        if (hurt) {
+            renderer.material.SetColor("_Color", Color.red);
+            hurt = false;
+        } else {
+            renderer.material.SetColor("_Color", prevColor);
+        }
     }
 }
