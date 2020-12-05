8938a1bf6d858b2adc5f5e35c0708ff6707642a6
gonzalo1@uw.edu <gonzalo1@uw.edu>
2020-12-04 19:38:50 -0800

    after boss defeat the statistics scene will load

--- Assets/Scripts/Boss/Boss_Health.cs
+++ Assets/Scripts/Boss/Boss_Health.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Boss_Health : MonoBehaviour {
     public float bossHealth = 160f;
@@ -14,63 +15,64 @@ public class Boss_Health : MonoBehaviour {
     GameObject head;
 
     // Start is called before the first frame update
-    void Start() {
-        Debug.Assert(player != null);
-        head = GameObject.Find("Head");
-        var renderer = head.GetComponent<Renderer>();
-        prevColor = renderer.material.GetColor("_Color");
+    void Start () {
+        Debug.Assert (player != null);
+        head = GameObject.Find ("Head");
+        var renderer = head.GetComponent<Renderer> ();
+        prevColor = renderer.material.GetColor ("_Color");
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update () {
         if (bossHealth == 100 && !special1) {
-            StartCoroutine(Timer());
-            DropSword();
+            StartCoroutine (Timer ());
+            DropSword ();
             flyTimer = 4f;
         }
         if (bossHealth == 80) {
-            animator.SetBool("IsEnraged", true);
+            animator.SetBool ("IsEnraged", true);
         }
         if (flyTimer <= 0) {
-            animator.SetBool("Special1", false);
+            animator.SetBool ("Special1", false);
         }
         flyTimer -= Time.deltaTime;
 
         if (bossHealth == 0) {
-            Die();
+            Die ();
         }
-        HitColor();
+        HitColor ();
     }
-    public void Die() {
-        player.GetComponent<Player>().EndOfLevel();
-        Destroy(gameObject, 5f);
+    public void Die () {
+        player.GetComponent<Player> ().EndOfLevel ();
+        Destroy (gameObject, 5f);
         bossHealth -= 10;
-        animator.SetTrigger("Die");
+        animator.SetTrigger ("Die");
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
     }
-    public void DropSword() {
-        animator.SetBool("Special1", true);
+    public void DropSword () {
+        animator.SetBool ("Special1", true);
         special1 = true;
-        Timer();
+        Timer ();
     }
-    IEnumerator Timer() {
-        yield return new WaitForSeconds(2.0f);
-        swordTrap.GetComponent<BossSwordTrap>().Drop();
+    IEnumerator Timer () {
+        yield return new WaitForSeconds (2.0f);
+        swordTrap.GetComponent<BossSwordTrap> ().Drop ();
     }
-    public void Damage(AttackDetails damageDetail) {
+    public void Damage (AttackDetails damageDetail) {
         bossHealth -= damageDetail.damageAmount;
         hurt = true;
-        var renderer = head.GetComponent<Renderer>();
-        prevColor = renderer.material.GetColor("_Color");
-        HitColor();
+        var renderer = head.GetComponent<Renderer> ();
+        prevColor = renderer.material.GetColor ("_Color");
+        HitColor ();
     }
 
-    public void HitColor() {
-        var renderer = head.GetComponent<Renderer>();
+    public void HitColor () {
+        var renderer = head.GetComponent<Renderer> ();
         if (hurt) {
-            renderer.material.SetColor("_Color", Color.red);
+            renderer.material.SetColor ("_Color", Color.red);
             hurt = false;
         } else {
-            renderer.material.SetColor("_Color", prevColor);
+            renderer.material.SetColor ("_Color", prevColor);
         }
     }
 }
